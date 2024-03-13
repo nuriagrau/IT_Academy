ce2290c5cfab44b2e9220a91d74f2caa5c11f76f
nuriagrau <nuriagrau@protonmail.com>
2024-03-13 12:48:58 +0100

    Primer Commit

---
+++ n2exercici1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Smartphone fairphone5 = new Smartphone("Fairphone", "5");
+
+        fairphone5.trucar("900 533 175");
+        fairphone5.fotografiar();
+        fairphone5.alarma();
+
+    }
+}
---
+++ n2exercici1/Rellotge.java
@@ -0,0 +1,3 @@
+public interface Rellotge {
+    public void alarma();
+}
